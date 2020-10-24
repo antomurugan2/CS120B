888fc6f512d29fc7ee3a94e0f75315aa2c21e89c
antomurugan2 <63279320+antomurugan2@users.noreply.github.com>
2020-10-24 13:16:05 -0700

    Ex 1 draft

--- Lab4_stateMachines/source/main.c
+++ Lab4_stateMachines/source/main.c
@@ -11,13 +11,44 @@
 #ifdef _SIMULATE_
 #include "simAVRHeader.h"
 #endif
+#include "RIMS.h"
 
-int main(void) {
-    /* Insert DDR and PORT initializations */
+enum SM3_STATES { SM3_SMStart, SM3_OffR, SM3_OnR, SM3_OffP, SM3_OnP} SM3_STATE;
 
-    /* Insert your solution below */
-    while (1) {
+void Tick_OffOnRP() {
+ switch(SM3_STATE) { // Transitions
+    case SM3_SMStart: // Initial transition
+                    SM2_STATE = SM3_OffR;
+                    B = 0;
+                    break;
+    case SM3_OffR:
+                    if (A0){SM3_STATE = SM3_OnP; B = 0x1;}
+                    else if (!A0){SM3_STATE = SM3_OffR;}
+                    else {SM3_STATE = SM3_OffR;}
+                    break;
+    case SM3_OnP:
+                    if (!A0) {SM3_STATE = SM3_OnR;}
+                    else if (A0) {SM3_STATE = SM3_OnP;}
+                    else {SM3_STATE = SM3_OnP;}
+                    break;
+    case SM3_OnR:
+                    if (A0){SM3_STATE = SM3_OffP; B = 0;}
+                    else if (!A0){SM3_STATE = SM3_OnR;}
+                    else {SM3_STATE = SM3_OnR;}
+                    break;
+    case SM3_OffP:
+                    if (!A0) {SM3_STATE = SM3_OffR;}
+                    else if (A0) {SM3_STATE = SM3_OffP;}
+                    else {SM3_STATE = SM3_OffP;}
+                    break;
+    default:
+                    SM3_STATE = SM3_OffR;
+                    break;
+   } // Transitions
+}
 
-    }
-    return 1;
+void main() {
+ B = 0x00; // Initialize outputs
+ SM3_STATE = SM3_SMStart; // Indicates initial call
+ while(1) {Tick_OffOnRP();}
 }
